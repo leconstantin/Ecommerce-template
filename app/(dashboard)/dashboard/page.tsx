7553d3e45fb84590e6d7bc0a3ee0970ab9c0f14c
Leo Constantin <lcon69184@gmail.com>
2025-10-08 10:53:55 +0200

    added folder strucucture

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard Page",
+  description: "Dashboard",
+};
+
+export default function DashboardPage() {
+  return <div>Dashboard Page</div>;
+}
